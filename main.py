75c22497f077f62685a4def3862173323a89db61
Aiting Lee <87257634+AitingLee@users.noreply.github.com>
2025-04-28 22:30:10 +0200

    Delete obsolete file and rearrange folder

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from config import SOURCE_FOLDER, WEB_REPO_URL, API_REPO_URL, ENDPOINTS_FOLDER, CALLERS_FOLDER
+from common.config import SOURCE_FOLDER, WEB_REPO_URL, API_REPO_URL, ENDPOINTS_FOLDER, CALLERS_FOLDER
 from data_gather.callers_builder import build_api_caller_dictionary
 from data_gather.endpoints_builder import build_api_endpoints_dictionary
-from repo_donwloader import download_repo
+from common.repo_donwloader import download_repo
 from visualization.graph_builder import build_graph, draw_graph
 
 download_repo(SOURCE_FOLDER,'zeeguu_web', WEB_REPO_URL)
